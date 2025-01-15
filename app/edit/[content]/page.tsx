b368f5d18365c1d6615184bf18e14b6ccc5c15f2
chandan yadav <nippiyadav890@gmail.com>
2025-01-15 07:50:43 +0530

    yes change in the edit

--- app/edit/[content]/page.tsx
+++ app/edit/[content]/page.tsx
@@ -2,48 +2,52 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import ContentEditor from '@/app/components/ContendEditor';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '@/lib/readux/store';
-import { singleArticleEditFetching } from '@/lib/readux/editContent';
 
-export interface PropsContentEdit{
+export interface PropsContentEdit {
   blogImageUrl?: string;
   category?: string;
   slug?: string;
   content?: string;
   alt?: string;
-  title?:string;
-  description?:string;
-  featuredImagealt?:string;
-  _id?:string;
+  title?: string;
+  description?: string;
+  featuredImagealt?: string;
+  _id?: string;
 }
 
 const ContentEdit = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  
-  const { content } = useParams(); // Await params to extract the `content` property
-  
-  console.log(content); // This will log the `content` from the dynamic route
-  
-  const {article,error,loading} = useSelector((state:RootState)=> state.edit);
-  console.log(article); // This will log the `content` from the dynamic route
-  const [contentObj,setContent] = useState<PropsContentEdit>(article as PropsContentEdit );
 
-  useEffect(()=>{
-    const response = async ()=>{
-     try {
-      dispatch(singleArticleEditFetching({id:content as string}))
- 
-      
-     } catch (error) {
-      console.log("Error in Fetching article in the edit:- ", error);
-     }
+  const { content } = useParams(); // Await params to extract the `content` property
+
+  console.log(content); // This will log the `content` from the dynamic route
+
+  const [contentObj, setContent] = useState<PropsContentEdit>();
+
+
+  useEffect(() => {
+    console.log("inside the dedit useEffect");
+    
+    const response = async () => {
+      try {
+        const response = await fetch(`/edit/content/api?contentId=${content}&content=Get`);
+
+        if (response.ok) {
+          const jsonConverted = await response.json();
+          console.log(jsonConverted);
+          if (jsonConverted?.data[0]) {
+            setContent(jsonConverted?.data[0])
+          }
+
+        }
+      } catch (error) {
+        console.log("Error in Fetching article in the edit:- ", error);
+      }
     }
-    response()
-  },[content])
-  
+    response();
+  }, [content])
+
   return (
-    <div>{contentObj? <ContentEditor {...contentObj}/> : <></>}</div>
+    <div>{contentObj ? <ContentEditor {...contentObj} /> : <></>}</div>
   )
 }
 
