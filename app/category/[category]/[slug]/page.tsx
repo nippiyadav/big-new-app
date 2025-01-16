73944149f15fed6cefc6b344aab0e195c8a68315
chandan yadav <nippiyadav890@gmail.com>
2025-01-16 12:48:45 +0530

    feeds included

--- app/category/[category]/[slug]/page.tsx
+++ app/category/[category]/[slug]/page.tsx
@@ -5,6 +5,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { singleArticleFetching, SingleArticleProps } from '@/lib/readux/singleArticleFetched';
 import { AppDispatch, RootState } from "@/lib/readux/store";
 import ArticleShowerComp from '@/app/components/ArticleShowerComponents/ArticleShowerComp';
+import { fetchCategories } from '@/lib/readux/categoryFetching';
+import BlogContainer from '@/app/components/BlogContainer';
+
 
 function ArticleShower() {
 const dispatch = useDispatch<AppDispatch>();
@@ -17,27 +20,45 @@ console.log(article,error,loading);
 
 
 useEffect(()=>{
-    const asyncFun = async()=>{
-        if (slug) {
+  const asyncFun = async()=>{
+    if (slug) {
             const id = slug.split("-");
             const lengthMin = id.length - 1;
             console.log(id[lengthMin]);
             const articleData = {
-                id :id[lengthMin],
-                slug: slug
+              id :id[lengthMin],
+              slug: slug
             };
             dispatch(singleArticleFetching(articleData))
+          }
         }
-    }
     asyncFun();
-},[slug])
+  },[slug])
+  
+
+  const {data:categoryCamp,loading:loadingTrue} = useSelector((state:RootState)=>state.category);
+
+  console.log(categoryCamp);
+  
+
+useEffect(()=>{
+  dispatch(fetchCategories({category:category}))
+},[category])
 
   return (
+    <>
     <section>
       {article.map((elem,index)=>
       <ArticleShowerComp key={index} {...elem as SingleArticleProps}/>
       )}
     </section>
+    <section className='max-w-screen-lg mx-auto px-3 mt-2'>
+      <h1 className='font-bold lg:text-4xl text-2xl text-center mb-2'>Related Articles {category.toUpperCase()}</h1>
+    {categoryCamp.map((elem, index) =>
+            <BlogContainer {...elem} key={index} className='' />
+          )}
+    </section>
+    </>
   )
 }
 
